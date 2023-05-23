6fe5636a5c467839ae927f76c22fc16582aad380
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-05-23 21:30:11 +0300

    change main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const theName = prompt('What is your name?');
 if (theName == 'John') {
    alert('Hello, John! How are you?');
    console.log('This is John');
-} else if (theName !== 'John') {
+} else if (theName == '') {
    alert('Hello, noname! How are you?');
    console.log('This is no name');
 }
