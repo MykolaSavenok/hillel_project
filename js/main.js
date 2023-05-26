958e52769c33a0888e01ace116ac1067cab6ad36
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-05-26 13:04:02 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-const input = +prompt('Enter a number from 1 to 4');
-switch (input) {
-   case 1:
-      alert('Today is a good day to start something');
-      break;
-   case 2:
-      alert('See your friend today');
-      break;
-   case 3:
-      alert('Buy yourself something tasty today');
-      break;
-   case 4:
-      alert('Do your HW today');
-      break;
-   default:
-      alert('Invalid number');
+// if else 
+const userName = prompt('What is your name?', '');
+if (userName === '' || userName === null) {
+   alert('Hello, noname! How are you?');
+   console.log('This is noname!');
+} else if (userName === userName) {
+   alert(`Hello, ${userName}! How are you?`);
+   console.log(`This is ${userName}`);
 }
+//////////////////////////////////////////////
+const userName2 = prompt('What is your name?');
+if (!userName2) {
+   alert('Hello, noname! How are you?');
+   console.log('This is noname!');
+} else {
+   alert(`Hello, ${userName2}! How are you?`);
+   console.log(`This is ${userName2}`);
+}
+//////////////////////////////////////////////
+const time = +prompt('Введіть кількість годин');
+let minutes = 60;
+let seconds = 60;
+let result = minutes * seconds;
+if (!time) {
+   alert('Invalid result');
+   console.log('Invalid result');
+} else {
+   alert(`You enter ${time * result} seconds`);
+   console.log(`User enter ${time * result} seconds`);
+}
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
